6fd5d0ab494ea41a9cd3f115df2b89f3c85cebbb
Pedro Henrique <pedro00dk@gmail.com>
2018-09-15 12:04:45 -0300

    Add color changing

--- public/sketch.js
+++ public/sketch.js
@@ -16,8 +16,9 @@ function setup() {
     createCanvas(window.innerWidth * 0.9, window.innerHeight * 0.9)
     background(0)
     noFill()
-    stroke(255)
     strokeWeight(0.1)
+
+    colorMode(HSB)
 }
 
 function draw() {
@@ -26,10 +27,11 @@ function draw() {
     for (let i = 1; i < yields.length; i++) {
         const current = yields[i]
         const previous = yields[i - 1]
-        
+
         let center = (current + previous) / 2
         let diameter = Math.abs(current - previous)
-        
+
+        stroke(i % 360, 100, 100)
         arc(center, height / 2, diameter, diameter, i % 2 == 0 ? 0 : Math.PI, i % 2 != 0 ? 0 : Math.PI)
     }
 }
