d45c3e37cd7ecbfe46c39e5171eadd665fbb5962
Pedro Henrique <pedro00dk@gmail.com>
2018-09-15 12:00:19 -0300

    Implement arc drawings

--- public/sketch.js
+++ public/sketch.js
@@ -15,13 +15,21 @@ const yields = []
 function setup() {
     createCanvas(window.innerWidth * 0.9, window.innerHeight * 0.9)
     background(0)
-
     noFill()
     stroke(255)
+    strokeWeight(0.1)
 }
 
 function draw() {
-    const next = generator.next().value
-    yields.push(next)
-    console.log(next)
+    yields.push(generator.next().value)
+
+    for (let i = 1; i < yields.length; i++) {
+        const current = yields[i]
+        const previous = yields[i - 1]
+        
+        let center = (current + previous) / 2
+        let diameter = Math.abs(current - previous)
+        
+        arc(center, height / 2, diameter, diameter, i % 2 == 0 ? 0 : Math.PI, i % 2 != 0 ? 0 : Math.PI)
+    }
 }
